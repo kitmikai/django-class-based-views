664205c2cfc20465c550095bb459f4ceae4a2543
kitmikai <tondieki@safaricom.co.ke>
2021-03-05 17:11:29 +0300

    Add BooksInline

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,22 +6,39 @@ from .models import Author, Genre, Book, BookInstance, Language
 # admin.site.register(Book)
 # admin.site.register(Author)
 # admin.site.register(BookInstance)
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
 admin.site.register(Language)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 # Register the admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 # Register the admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
