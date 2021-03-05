910f30e0636f2c7eda4c8e60ff0f9df4cb853dc8
kitmikai <tondieki@safaricom.co.ke>
2021-03-05 10:53:27 +0300

    Add project templates.

--- homelibrary/settings.py
+++ homelibrary/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'homelibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["homelibrary/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
