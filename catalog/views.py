19073326ae213eba2333be82498a16aed60ecc83
kitmikai <tondieki@safaricom.co.ke>
2021-03-07 04:08:01 +0300

    Add LoanedBooksLibrarianListView class.

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from catalog.models import Book, Author, BookInstance, Genre
+
 # Create your views here.
 
 @login_required
@@ -61,3 +63,25 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class LoanedBooksLibrarianListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing all books on loan for librarians."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_librarian.html'
+    paginate_by = 10
+    permission_required = 'catalog.add_bookinstance'
+
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+
